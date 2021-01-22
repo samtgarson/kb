7f036f3b8901d3b28ff78032bf966161874d305e
Sam Garson <samtgarson@gmail.com>
2021-01-22 18:19:32 +0000

    HTML diagrams

---
+++ docs/script.js
@@ -0,0 +1,685 @@
+const layout = [
+    {
+        width: 60,
+        height: 60,
+        top: 0,
+        left: 0
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 0,
+        left: 60
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 0,
+        left: 120
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 0,
+        left: 180
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 0,
+        left: 240
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 0,
+        left: 300
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 0,
+        left: 360
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 0,
+        left: 420
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 0,
+        left: 480
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 0,
+        left: 540
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 0,
+        left: 600
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 0,
+        left: 660
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 0,
+        left: 720
+    },
+    {
+        width: 120,
+        height: 60,
+        top: 0,
+        left: 780
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 60,
+        left: 0
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 60,
+        left: 60
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 60,
+        left: 120
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 60,
+        left: 180
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 60,
+        left: 240
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 60,
+        left: 300
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 60,
+        left: 360
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 60,
+        left: 420
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 60,
+        left: 480
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 60,
+        left: 540
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 60,
+        left: 600
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 60,
+        left: 660
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 60,
+        left: 720
+    },
+    {
+        width: 60,
+        height: 120,
+        top: 60,
+        left: 780
+    },
+    {
+        width: 65,
+        height: 65,
+        top: 57.5,
+        left: 837.5,
+        round: true
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 120,
+        left: 0
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 120,
+        left: 60
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 120,
+        left: 120
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 120,
+        left: 180
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 120,
+        left: 240
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 120,
+        left: 300
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 120,
+        left: 360
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 120,
+        left: 420
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 120,
+        left: 480
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 120,
+        left: 540
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 120,
+        left: 600
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 120,
+        left: 660
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 120,
+        left: 720
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 180,
+        left: 0
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 180,
+        left: 60
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 180,
+        left: 120
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 180,
+        left: 180
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 180,
+        left: 240
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 180,
+        left: 300
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 180,
+        left: 360
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 180,
+        left: 420
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 180,
+        left: 480
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 180,
+        left: 540
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 180,
+        left: 600
+    },
+    {
+        width: 120,
+        height: 60,
+        top: 180,
+        left: 660
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 180,
+        left: 780
+    },
+    {
+        width: 75,
+        height: 60,
+        top: 240,
+        left: 0
+    },
+    {
+        width: 75,
+        height: 60,
+        top: 240,
+        left: 75
+    },
+    {
+        width: 75,
+        height: 60,
+        top: 240,
+        left: 150
+    },
+    {
+        width: 75,
+        height: 60,
+        top: 240,
+        left: 225
+    },
+    {
+        width: 120,
+        height: 60,
+        top: 240,
+        left: 300
+    },
+    {
+        width: 75,
+        height: 60,
+        top: 240,
+        left: 420
+    },
+    {
+        width: 75,
+        height: 60,
+        top: 240,
+        left: 495
+    },
+    {
+        width: 75,
+        height: 60,
+        top: 240,
+        left: 570
+    },
+    {
+        width: 75,
+        height: 60,
+        top: 240,
+        left: 645
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 240,
+        left: 720
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 240,
+        left: 780
+    },
+    {
+        width: 60,
+        height: 60,
+        top: 240,
+        left: 840
+    }
+]
+
+const keys = [
+    [
+        "ESC",
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "0",
+        "-",
+        "+",
+        "BKSPCE",
+
+        "TAB",
+        "Q",
+        "W",
+        "E",
+        "R",
+        "T",
+        "Y",
+        "U",
+        "I",
+        "O",
+        "P",
+        "[",
+        "]",
+        "ENTER",
+        "SCRL",
+
+        "CAPS",
+        "A",
+        "S",
+        "D",
+        "F",
+        "G",
+        "H",
+        "J",
+        "K",
+        "L",
+        ";",
+        "'",
+        "\\",
+
+        "LSHIFT",
+        "Z",
+        "X",
+        "C",
+        "V",
+        "B",
+        "N",
+        "M",
+        ",",
+        ".",
+        "/",
+        "RSHIFT",
+        "UP",
+
+        "FN",
+        "LCTRL",
+        "LALT",
+        "LGUI",
+        "SPACE",
+        "RGUI",
+        "RALT",
+        "RCTRL",
+        "FN",
+        "LEFT",
+        "DOWN",
+        "RIGHT"
+    ],
+    [
+        "SLEEP",
+        "F1",
+        "F2",
+        "F3",
+        "F4",
+        "F5",
+        "F6",
+        "F7",
+        "F8",
+        "F9",
+        "F10",
+        "F11",
+        "F12",
+        "DEL",
+
+        "RESET",
+        "BT 1",
+        "BT 2",
+        "BT 3",
+        "BT 4",
+        "BT 5",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "VOL",
+
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "PGUP",
+
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "HOME",
+        "PGDN",
+        "END"
+    ]
+]
+
+const coords = [
+    	[0,0],  [0,1],  [0,2],  [0,3],  [0,4],  [0,5],  [0,6],  [0,7],  [0,8],  [0,9],  [0,10], [0,11], [0,12],     [0,13],
+		[1,0],  [1,1],  [1,2],  [1,3],  [1,4],  [1,5],  [1,6],  [1,7],  [1,8],  [1,9],  [1,10], [1,11], [1,12], [2,13], [1,13],
+		[2,0],  [2,1],  [2,2],  [2,3],  [2,4],  [2,5],  [2,6],  [2,7],  [2,8],  [2,9],  [2,10], [2,11], [2,12],
+		[3,0],  [3,1],  [3,2],  [3,3],  [3,4],  [3,5],  [3,6],  [3,7],  [3,8],  [3,9],  [3,10],      [3,11],    [3,13],
+		    [4,0],    [4,1],    [4,2],    [4,4],      [4,5],      [4,7],    [4,8],   [4,9],   [4,10],   [4,11], [4,12], [4,13],
+
+]
+
+const key = ({ top, left, width, height, round}, text) => {
+    const div = document.createElement('div');
+    div.innerHTML = `<div class="display-key" style="top: ${top}px; left: ${left}px;">
+        <div class="display-key-block" style="width: ${width}px; height: ${height}px;">
+            <div class="display-key-block-inner ${round ? "round" : ""}">${text}</div>
+        </div>
+    </div>`
+    return div
+}
+
+const createWrapper = () => {
+    const div = document.createElement('div')
+    div.className = "display"
+    div.style = `width: ${BOX_WIDTH}px; height: ${BOX_HEIGHT}px; margin-top: 0px;`
+    return div
+}
+
+const insertWrapper = (wrapper, text) => {
+    const title = document.createElement('h2')
+    title.textContent = text
+    document.body.appendChild(title)
+
+    document.body.appendChild(wrapper)
+}
+
+const generateKeyMap = () =>
+    keys.forEach((layer, layerIndex) => {
+        const wrapper = createWrapper()
+        layout.forEach((position, index) => {
+            const div = key(position, layer[index])
+            wrapper.appendChild(div)
+        })
+        
+        insertWrapper(wrapper, `Layer ${layerIndex}`)
+    })
+
+const ROWS = 5
+const COLS = 14
+const POINT_SIZE = 4
+const BOX_WIDTH = 900;
+const BOX_HEIGHT = 300;
+
+const getCenter = ({ top, left, height, width}) => [left + width/2, top + height/2];
+
+const createPoint = ({ width, height, top, left }) => {
+    const div = document.createElement('div')
+    div.className = "point"
+    const [x, y] = getCenter({ width, height, top, left })
+    div.style = `left: ${x - POINT_SIZE/2}px; top: ${y - POINT_SIZE/2}px;`
+    return div
+}
+
+
+const XMLNS = "http://www.w3.org/2000/svg";
+const findTarget = (row, col, dir) => {
+    const [x, y] = dir === "row" 
+        ? [row, col + 1]
+        : [row + 1, col]
+
+    const targetIndex = coords.findIndex(c => c[0] == x && c[1] == y)
+    if (targetIndex < 0) {
+        if (x == ROWS || y == COLS) return
+        return findTarget(x, y, dir)
+    }
+    return layout[targetIndex]
+}
+
+const createWire = (position, row, col, dir) => {
+    const target = findTarget(row, col, dir)
+    if (!target) return
+    const line = document.createElementNS(XMLNS, "line")
+    line.setAttributeNS(null, "class", dir);
+
+    [position, target].forEach((pos, i) => {
+        const [x, y] = getCenter(pos)
+        line.setAttributeNS(null, `x${i+1}`, x)
+        line.setAttributeNS(null, `y${i+1}`, y)
+    })
+
+    return line
+}
+
+const createSvg = () => {
+    const svgElem = document.createElementNS(XMLNS, "svg");
+    svgElem.setAttributeNS(null, "viewBox", "0 0 " + BOX_WIDTH + " " + BOX_HEIGHT);
+    svgElem.setAttributeNS(null, "width", BOX_WIDTH);
+    svgElem.setAttributeNS(null, "height", BOX_HEIGHT);
+    svgElem.setAttributeNS(null, "class", "wiring-svg")
+    return svgElem
+}
+
+const generateWiring = () => {
+    const wrapper = createWrapper()
+    const svg = createSvg()
+
+    layout.forEach((position, index) => {
+        const div = key(position, "")
+        wrapper.appendChild(div)
+
+        wrapper.appendChild(createPoint(position))
+
+        const [row, col] = coords[index]
+        const rowLine = createWire(position, row, col, "col")
+        const colLine = createWire(position, row, col, "row")
+        if (rowLine) svg.appendChild(rowLine)
+        if (colLine) svg.appendChild(colLine)
+    })
+
+    wrapper.appendChild(svg);
+    insertWrapper(wrapper, "Wiring")
+}
+
+const execute = () => {
+    generateKeyMap()
+    generateWiring()
+}
+    
+
+
+
+document.addEventListener("DOMContentLoaded", execute)
